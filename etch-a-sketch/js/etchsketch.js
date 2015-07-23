e0d6c42b4c6312473991f765d35857087d53086f
Mo Amin <m.alamin92@gmail.com>
2015-07-23 04:53:27 -0400

    added the sketch functionality, when the mouse enters a grid it changes colors

--- etch-a-sketch/js/etchsketch.js
+++ etch-a-sketch/js/etchsketch.js
@@ -3,15 +3,18 @@ $(document).ready(function(){
 	var height = 40;
 	var width = 40;
 
-	$('#container').height(height*suareside);
-	$('#container').width(width*suareside);
+	$('.container').height(height*suareside);
+	$('.container').width(width*suareside);
 
 	for(var rows = 0; rows < width; rows++){
-	$('<div class="gridSquare"></div>').appendTo('#container')
+	$('<div id="gridSquare"></div>').appendTo('.container')
 		for(var cols = 0; cols < height; cols++){
-			$('<div class="gridSquare"></div>').appendTo('#container')
+			$('<div id="gridSquare"></div>').appendTo('.container')
 		}
 	}
 
+	$('.container').on('mouseenter', '#gridSquare', function(){
+		$(this).css('background-color', 'white');
+	});
 
 });
