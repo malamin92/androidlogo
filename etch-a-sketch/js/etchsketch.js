7c4be39615e3d9c85b902e5cd592905b8c60d87f
Mo Amin <m.alamin92@gmail.com>
2015-07-24 02:09:48 -0400

    made fixes on random colors, added styling to make it look like etch-a-sketch

--- etch-a-sketch/js/etchsketch.js
+++ etch-a-sketch/js/etchsketch.js
@@ -12,6 +12,14 @@ $('#reset').click(function(){
 		setup(userChoice);
 });
 
+$('#randomize').click(function(){
+
+		$('.container').empty();
+
+		userChoice = prompt("Enter how many boxes you want in each row and column (Less than 120):");
+		randomSetup(userChoice);
+});
+
 function setup(numOfSquares){
 
 	var numSquares = numOfSquares;
@@ -26,6 +34,37 @@ function setup(numOfSquares){
 
 	$('.container').on('mouseenter', '.gridSquare', function(){
 		$(this).css('background-color', 'black');
+	});
+
+	$('.gridSquare').width(squareSide);
+	$('.gridSquare').height(squareSide);
+
+}
+
+function randomColor(){
+
+	return Math.random() * (255 - 0) + 1;
+}
+
+function randomSetup(numOfSquares){
+
+	var numSquares = numOfSquares;
+	var squareSide = 500 / numSquares;
+	var totalSquares = numSquares * numSquares;
+
+
+	for(var rows = 0; rows < totalSquares; rows++){
+	$('<div class="gridSquare"></div>').appendTo('.container')
+	}
+    
+	$('.container').on('mouseenter', '.gridSquare', function(){
+		
+        var colors = [];
+        color1 = Math.round(randomColor());
+        color2 = Math.round(randomColor());
+        color3 = Math.round(randomColor());       
+        
+        $(this).css('background-color', 'rgb('+color1+','+color2+','+color3+')');
 	});
 
 	$('.gridSquare').width(squareSide);
