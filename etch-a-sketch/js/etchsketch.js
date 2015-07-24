2ee35a27f74bc632daf8db7cc1c5727f3a2d89cb
Mo Amin <m.alamin92@gmail.com>
2015-07-23 23:42:22 -0400

    added a funtiocal reset button to etch a sketch

--- etch-a-sketch/js/etchsketch.js
+++ etch-a-sketch/js/etchsketch.js
@@ -1,35 +1,34 @@
 $(document).ready(function(){
-	var numSquares = 20;
-	var squareSide = 660 / numSquares;
+
+	setup(2);	
+
+});
+
+$('#reset').click(function(){
+
+		$('.container').empty();
+
+		userChoice = prompt("Enter how many boxes you want in each row and column:");
+		setup(userChoice);
+});
+
+function setup(numOfSquares){
+
+	var numSquares = numOfSquares;
+	var squareSide = 600 / numSquares;
+	var totalSquares = numSquares * numSquares;
 
 
-	console.log(squareSide);
-
-	for(var rows = 0; rows < numSquares; rows++){
+	for(var rows = 0; rows < totalSquares; rows++){
 	$('<div class="gridSquare"></div>').appendTo('.container')
-		for(var cols = 0; cols < numSquares; cols++){
-			$('<div class="gridSquare"></div>').appendTo('.container')
-		}
 	}
 
 
 	$('.container').on('mouseenter', '.gridSquare', function(){
-		$(this).css('background-color', 'white');
+		$(this).css('background-color', '#707070');
 	});
 
 	$('.gridSquare').width(squareSide);
 	$('.gridSquare').height(squareSide);
 
-	$('#reset').click(function(){
-
-
-	});
-
-	funtion
-});
-
-
-function setup(){
-
-	
 }
